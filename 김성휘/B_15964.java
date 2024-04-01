92885b4ffae80e3f27fc56becf563135c00589a7
hwi-hwi-hwi <164748897+hwi-hwi-hwi@users.noreply.github.com>
2024-04-02 03:08:33 +0900

    Add files via upload

---
+++ 김성휘/B_15964.java
@@ -0,0 +1,26 @@
+package 제출X;
+
+import java.util.Scanner;
+
+public class B_15964 {
+	public static void A_B() {  // A@B 함수 선언
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		// 처음에 int로 해서 30점 나옴
+		// 범위를 보니 10^9 넘을 수 있으므로 Long을 사용
+		long A = scanner.nextLong();
+		long B = scanner.nextLong();
+		
+		long C = (A + B) * (A - B);
+		
+		System.out.print(C);
+	}
+	
+	public static void main(String[] args) {
+		
+		A_B();
+		
+	}
+
+}
---
+++ 김성휘/B_2558.java
@@ -0,0 +1,18 @@
+package 제출X;
+
+import java.util.Scanner;
+
+public class B_2558 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		scanner.close();
+		
+		System.out.print(a + b);
+	}
+
+}
