5b21b59b58581d96a471ece0727dc1f15a1d91e2
Teo <120327265+singsingsing3@users.noreply.github.com>
2024-05-14 00:14:01 +0900

    24-05-13 백준 2108

---
+++ 이태호/2108.py
@@ -0,0 +1,31 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+num = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+
+num.sort()
+total = sum(num)
+
+temp = {}
+for i in num:
+    if i in temp:
+        temp[i] += 1
+    else:
+        temp[i] = 1
+
+max_value = max(temp.values())
+numbers = [key for key, value in temp.items() if value == max_value]
+
+if len(numbers) == 1:
+    frequent = numbers[0]
+else:
+    frequent = sorted(numbers)[1]
+
+mean = round(total / N)
+median = num[(N - 1) // 2]
+rrange = num[-1] - num[0]
+
+print(mean)
+print(median)
+print(frequent)
+print(rrange)
