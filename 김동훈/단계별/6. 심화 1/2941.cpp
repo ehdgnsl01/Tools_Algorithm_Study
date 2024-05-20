28c949b2be3fc47187f80998e10e871edcf24990
ehdgnsl01 <gnsl505@naver.com>
2024-05-20 17:26:41 +0900

    update 240520

--- 김동훈/단계별/6. 심화 1/2941.cpp
+++ 김동훈/단계별/6. 심화 1/2941.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string str;
+int res = 0;
+
+void findcroatia(string s){
+    size_t pos = str.find(s);
+    while(pos != string::npos){
+        res--;
+        pos = str.find(s,pos + 1);
+    }
+}
 int main(){
-    char c_alpha[8]={'c=','c-','dz=','d-','lj','nj','s=','z='};
-    
+    cin >> str;
+    res = str.length();
+
+    findcroatia("c=");
+    findcroatia("c-");
+    findcroatia("dz=");
+    findcroatia("d-");
+    findcroatia("lj");
+    findcroatia("nj");
+    findcroatia("s=");
+    findcroatia("z=");
+
+    cout << res ;
+
 }
