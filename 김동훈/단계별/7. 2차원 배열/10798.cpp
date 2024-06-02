b7ad293c2b6d888a2ec14408d482ca6b90ff36c7
ehdgnsl01 <gnsl505@naver.com>
2024-06-02 17:37:35 +0900

    update 240602

--- 김동훈/단계별/7. 2차원 배열/10798.cpp
+++ 김동훈/단계별/7. 2차원 배열/10798.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
+char arr[6][16];
+
 int main(){
-    
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<15;i++){
+        for(int j=0;j<5;j++){
+            if(arr[j][i] != NULL){
+                cout << arr[j][i];
+            }
+        }
+    }
+
 }
